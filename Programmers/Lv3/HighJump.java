f9f4ef04113e10423b08a7582d033c2c24aacce1
ParkYoungJun <youngjun108059@gmail.com>
2020-03-01 21:37:35 +0900

    [Update] Algorithm Solving (멀리 뛰기)
    프로그래머스 Lv3

---
+++ Programmers/Lv3/HighJump.java
@@ -0,0 +1,24 @@
+package programmers;
+// LV3 ¸Ö¸® ¶Ù±â
+// 1Ä­ or 2Ä­¾¿ ¶Ù¾î¼­ nÄ­À» °¥ ¼ö ÀÖ´Â °æ¿ìÀÇ ¼ö ±¸ÇÏ±â
+public class HighJump {
+
+	public static void main(String[] args) {
+		int n = 5;
+		solution(n);
+	}
+	
+	static long solution(int n) {
+	    long answer = 0;
+	    int[] dp = new int[2000];
+	    dp[0] = 1;
+	    dp[1] = 2;
+	    for (int i = 2; i < n; i++) {
+			dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+		}
+	    answer = dp[n-1] % 1234567;
+	    System.out.println(answer);
+	    return answer;
+	}
+
+}
