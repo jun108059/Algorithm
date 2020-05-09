008945d9859440632b3c69f03693362f3ec7d166
YoungJun Park <youngjun108059@gmail.com>
2020-05-10 02:23:45 +0900

    [Solved] 09.Valid-Perfect-Square - LeetCode
    Valid-Perfect-Square Solving

---
+++ LeetCode/MayLeetCodingChallenge/Week2/09.Valid-Perfect-Square.java
@@ -0,0 +1,14 @@
+// Valid-Perfect-Square.java
+
+class Solution {
+    public boolean isPerfectSquare(int n) {
+        long sum = 0;
+        for (int i = 1; sum < n; i = i + 2) {
+            sum = sum + i;
+            if (sum == n) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
