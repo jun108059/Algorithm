7fd250280cc5b724836edd7524621c61d1b6f4ed
YoungJun_Park <youngjun108059@gmail.com>
2020-12-31 22:56:43 +0900

    [Solved] 반복되지 않는 문자 찾기

---
+++ SpartaAlgorithm/week_1/01_06_find_not_repeating_first_character.py
@@ -0,0 +1,32 @@
+# 2020.12.31
+# 반복되지 않는 문자
+# 영문 문자열에서 반복되지 않는 첫번째 문자를 return
+
+input = "abadabac"
+
+
+def find_not_repeating_first_character(string):
+    alphabet_occurrence_array = [0] * 26
+
+    for char in string:
+        if not char.isalpha():
+            continue
+        arr_index = ord(char) - ord("a")
+        alphabet_occurrence_array[arr_index] += 1
+
+    not_repeating_character_array = []
+    for index in range(len(alphabet_occurrence_array)):
+        alphabet_occurrence = alphabet_occurrence_array[index]
+
+        if alphabet_occurrence == 1:
+            not_repeating_character_array.append(chr(index + ord("a")))
+
+    for char in string:
+        if char in not_repeating_character_array:
+            return char
+
+    return "_"
+
+
+result = find_not_repeating_first_character(input)
+print(result)
