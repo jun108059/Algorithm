27f8aea2a84041b28f9c2181901d11623a661297
YoungJun_Park <youngjun108059@gmail.com>
2021-01-02 01:00:54 +0900

    [Solved] 문자열 뒤집기 문제

---
+++ SpartaAlgorithm/week_1/02_homework.py
@@ -0,0 +1,28 @@
+# 2021.01.02
+# Q2. 문자열 뒤집기
+# 0과 1로의 문자열에서 문자를 모두 0 또는 1로 같게 만들어보자.
+
+input = "011110"
+
+
+def find_count_to_turn_out_to_all_zero_or_all_one(string):
+    count_to_all_zero = 0
+    count_to_all_one = 0
+
+    if string[0] == '0':
+        count_to_all_one += 1
+    elif string[0] == '1':
+        count_to_all_zero += 1
+
+    for i in range(len(string) - 1):
+        if string[i] != string[i + 1]:
+            if string[i + 1] == '0':
+                count_to_all_one += 1
+            if string[i + 1] == '1':
+                count_to_all_zero += 1
+
+    return min(count_to_all_one, count_to_all_zero)
+
+
+result = find_count_to_turn_out_to_all_zero_or_all_one(input)
+print(result)
