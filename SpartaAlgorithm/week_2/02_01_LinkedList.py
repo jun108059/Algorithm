e1577f60117026028a96c757ecfd740a705a5365
YoungJun_Park <youngjun108059@gmail.com>
2021-01-03 22:06:49 +0900

    [Solved] LinkedList 구현

---
+++ SpartaAlgorithm/week_2/02_01_LinkedList.py
@@ -0,0 +1,26 @@
+# LinkedList 구현
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+# 3을 가진 Node 를 만드려면 아래와 같이 하면 됩니다!
+node = Node(3) # 현재는 next 가 없이 하나의 노드만 있습니다. [3]
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)  # head 에 시작하는 Node 를 연결합니다.
+
+    def append(self, value):     # LinkedList 가장 끝에 있는 노드에 새로운 노드를 연결합니다.
+        cur = self.head         
+        while cur.next is not None: # cur의 다음이 끝에 갈 때까지 이동합니다. 
+            cur = cur.next          
+        cur.next = Node(value)
+
+
+linked_list = LinkedList(5)
+linked_list.append(12)
+# 이렇게 되면 5 -> 12 형태로 노드를 연결한 겁니다!
+linked_list.append(8)
+# 이렇게 되면 5 -> 12 -> 8 형태로 노드를 연결한 겁니다!
