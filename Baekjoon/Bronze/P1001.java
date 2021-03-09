ae588c6518c8a7db54edb06ccb76fb8ab0a01abc
YoungJun_Park <youngjun108059@gmail.com>
2021-03-10 00:08:24 +0900

    [Solved] 백준 브론즈 문제 풀이
    완전 기초부터 풀어보기

---
+++ Baekjoon/Bronze/P1001.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+/**
+ * 백준 #1000 : A-B
+ * https://www.acmicpc.net/problem/1001
+ *
+ */
+public class P1001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A - B);
+    }
+}
