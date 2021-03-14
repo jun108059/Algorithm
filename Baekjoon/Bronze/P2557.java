87f11b65e48825049a6bfa836f645b99edc95e41
YoungJun_Park <youngjun108059@gmail.com>
2021-03-15 00:33:24 +0900

    [Solved] 백준 : #2557 (분류) Hello World!

---
+++ Baekjoon/Bronze/P2557.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+/**
+ * 백준 #2557 : HelloWorld (브론즈5)
+ * https://www.acmicpc.net/problem/2557
+ *
+ * Hello World 출력
+ */
+public class P2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
