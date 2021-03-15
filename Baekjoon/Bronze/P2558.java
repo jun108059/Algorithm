0981023f42732f76758d74a9c0ed28864255c8bb
YoungJun_Park <youngjun108059@gmail.com>
2021-03-16 01:22:39 +0900

    [Solved] 백준 : #2558 (분류) A+B -2
    브론즈 5

---
+++ Baekjoon/Bronze/P2558.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.*;
+
+/**
+ * 백준 #2558 : A+B -2
+ * https://www.acmicpc.net/problem/2558
+ *
+ */
+public class P2558 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A + B);
+    }
+}
+
