1697805aa15f5df0fff83b1979e7bbd162dad4a2
YoungJun_Park <youngjun108059@gmail.com>
2021-03-18 00:06:36 +0900

    [Solved] 백준 : #5337 (분류) 웰컴
    브론즈5

---
+++ Baekjoon/Bronze/P5337.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+/**
+ * 백준 #5337 : 웰컴 (브론즈5:분류)
+ * https://www.acmicpc.net/problem/5337
+ *
+ * Welcome 출력
+ */
+public class P5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   . ");
+        System.out.println("|  | _ | _. _ ._ _  _ ");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
