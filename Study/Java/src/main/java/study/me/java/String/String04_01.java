b307a94474227a6c255173084a8ec41f17ad0445
YoungJun Park <youngjun108059@gmail.com>
2022-01-24 23:36:39 +0900

    [Solved] 인프런 : #4 (String) 단어 뒤집기

---
+++ Study/Java/src/main/java/study/me/java/String/String04_01.java
@@ -0,0 +1,33 @@
+package study.me.java.String;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 04-1. 단어 뒤집기
+ */
+public class String04_01 {
+
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String04_01 T = new String04_01();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = kb.next();
+        }
+        for (String x : T.solution(n, str)) {
+            System.out.println(x);
+        }
+    }
+
+}
---
+++ Study/Java/src/main/java/study/me/java/String/String04_02.java
@@ -0,0 +1,42 @@
+package study.me.java.String;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 04-2. 단어 뒤집기
+ */
+public class String04_02 {
+
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length() - 1;
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String04_02 T = new String04_02();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = kb.next();
+        }
+        for (String x : T.solution(n, str)) {
+            System.out.println(x);
+        }
+    }
+
+}
