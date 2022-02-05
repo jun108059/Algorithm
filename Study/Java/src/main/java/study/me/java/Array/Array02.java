eba5ff3d5419bb8422d4dd6982dda802516e6745
YoungJun Park <youngjun108059@gmail.com>
2022-02-06 00:04:49 +0900

    [Solved] 인프런 : #14 (Array) 보이는 학생

---
+++ Study/Java/src/main/java/study/me/java/Array/Array02.java
@@ -0,0 +1,27 @@
+package study.me.java.Array;
+
+import java.util.Scanner;
+
+public class Array02 {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.print(solution(n, arr));
+    }
+
+    public static int solution(int n, int[] arr) {
+        int answer = 1, max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
