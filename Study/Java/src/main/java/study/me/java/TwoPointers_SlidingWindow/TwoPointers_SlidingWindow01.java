e358ebe39c0ab4399280374f1bfe81e32ff3a3a5
YoungJun Park <youngjun108059@gmail.com>
2022-02-20 22:53:14 +0900

    [Solved] 인프런 : #25 (TP_SW) 두 배열 합치기

---
+++ Study/Java/src/main/java/study/me/java/TwoPointers_SlidingWindow/TwoPointers_SlidingWindow01.java
@@ -0,0 +1,38 @@
+package study.me.java.TwoPointers_SlidingWindow;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 01. 두 배열 합치기
+ */
+public class TwoPointers_SlidingWindow01 {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = kb.nextInt();
+        }
+        int m = kb.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = kb.nextInt();
+        }
+        for (int x : solution(n, m, a, b)) System.out.print(x + " ");
+    }
+
+    public static ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (a[p1] < b[p2]) answer.add(a[p1++]);
+            else answer.add(b[p2++]);
+        }
+        while (p1 < n) answer.add(a[p1++]);
+        while (p2 < m) answer.add(b[p2++]);
+        return answer;
+    }
+
+}
