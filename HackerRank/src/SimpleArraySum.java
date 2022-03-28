1105ba26cb9ae67848fe7bb97c1ecd4bbd4e3021
YoungJun Park <youngjun108059@gmail.com>
2022-03-28 23:53:22 +0900

    [Solved] HackerRank : SimpleArraySum

---
+++ HackerRank/src/SimpleArraySum.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class SimpleArraySum {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.simpleArraySum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+    private static class Result {
+
+        /*
+         * Complete the 'simpleArraySum' function below.
+         *
+         * The function is expected to return an INTEGER.
+         * The function accepts INTEGER_ARRAY ar as parameter.
+         */
+        public static int simpleArraySum(List<Integer> ar) {
+            int sum = 0;
+            for (Integer integer : ar) {
+                sum += integer;
+            }
+            return sum;
+        }
+
+    }
+}
