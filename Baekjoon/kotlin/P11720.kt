205d63e2b8689e80aed1aa84e5f2abcc0b32d06d
YoungJun Park <youngjun108059@gmail.com>
2023-06-24 17:10:45 +0900

    📚 PS:배열/리스트(숫자의 합 구하기)
    https://www.acmicpc.net/problem/11720

---
+++ Baekjoon/kotlin/P11720.kt
@@ -0,0 +1,20 @@
+/**
+ * 숫자의 합 구하기(배열/리스트)11720
+ *
+ * - baekjoon 11720(https://www.acmicpc.net/problem/11720)
+ */
+fun main() {
+    val numberString = "54321"
+    val result = sumOfNumbers(numberString)
+    println(result)
+}
+
+fun sumOfNumbers(numberString: String) =
+    numberString.map { it.toString().toLong() }.sum()
+//    numberString.map { it - '0' }.sum()
+
+fun p11720() {
+    val unUseCount = readln().toInt()
+    val numberString = readln()
+    println(numberString.map { it.toString().toLong() }.sum())
+}
