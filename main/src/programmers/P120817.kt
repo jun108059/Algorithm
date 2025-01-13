235f6f66f2269f573441dd5c080aa1e72f9ab49a
YoungJun Park <youngjun108059@gmail.com>
2025-01-13 23:59:55 +0900

    📚 PS:프로그래머스 입문 코드 day4(4/4)
    Signed-off-by: YoungJun Park <youngjun108059@gmail.com>

---
+++ main/src/programmers/P120817.kt
@@ -0,0 +1,16 @@
+package programmers
+
+
+/**
+ * 배열의 평균값
+ * - [link](https://school.programmers.co.kr/learn/courses/30/lessons/120817)
+ * - 정수 배열 numbers의 원소의 평균값을 return
+ */
+class P120817 {
+    companion object {
+        // 정수배열의 평균값
+        fun solution(numbers: IntArray): Double {
+            return numbers.average()
+        }
+    }
+}
